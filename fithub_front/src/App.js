bc354a6754d4bc3df5ad5c157dad054205d3c648
hankee99 <hankee60@gmail.com>
2025-03-26 10:58:00 +0900

    feat : 메인컴포넌트 생성

--- fithub_front/src/App.js
+++ fithub_front/src/App.js
@@ -1,5 +1,18 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Main from "./component/common/Main";
+import MyFitMain from "./component/MyFit/MyFitMain";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrap">
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="myfit" element={<MyFitMain />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ fithub_front/src/component/common/Main.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>
+      <h1>메인페이지</h1>
+      <Link to="/myfit">My Fit</Link>
+    </div>
+  );
+};
+
+export default Main;
