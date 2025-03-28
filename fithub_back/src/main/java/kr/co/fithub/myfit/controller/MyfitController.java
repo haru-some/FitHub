47e67065343cc633ac56a39d0542d68da91609c3
hankee99 <hankee60@gmail.com>
2025-03-28 18:36:41 +0900

    feat : 루틴설정하고 db에 반영

--- fithub_back/src/main/java/kr/co/fithub/myfit/controller/MyfitController.java
+++ fithub_back/src/main/java/kr/co/fithub/myfit/controller/MyfitController.java
@@ -1,5 +1,9 @@
 package kr.co.fithub.myfit.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,6 +18,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @CrossOrigin("*")
@@ -34,12 +40,25 @@ public class MyfitController {
 	}
 	
 	@GetMapping(value="/routine/{memberNo}")
-	public ResponseEntity<Routine> selectRoutine(@PathVariable int memberNo, @RequestParam String routineDay) {
-		Routine routine = new Routine();
-		routine.setMemberNo(memberNo);
-		routine.setRoutineDay(routineDay);
-		Routine r = myfitService.selectRoutine(routine);
-		return ResponseEntity.ok(r);
+	public ResponseEntity<Object> selectRoutine(@PathVariable int memberNo, @RequestParam(required = false, defaultValue = "none") String routineDay) {
+		if(routineDay.equals("none")) {
+			HashMap<String, Routine> map = myfitService.selectRoutineList(memberNo);
+			return ResponseEntity.ok(map);
+		}else {
+			Routine routine = new Routine();
+			routine.setMemberNo(memberNo);
+			routine.setRoutineDay(routineDay);
+			Routine r = myfitService.selectRoutine(routine);
+			return ResponseEntity.ok(r);
+		}
+	}
+	
+	@PutMapping("/routine/{memberNo}")
+	public ResponseEntity<Integer> putMethodName(@PathVariable int memberNo, @RequestBody Map<String, String> routineMap) {
+		int result = myfitService.updateRoutine(memberNo,routineMap);
+		
+		
+		return ResponseEntity.ok(result);
 	}
 	
 
--- fithub_front/src/component/MyFit/MyFitMain.js
+++ fithub_front/src/component/MyFit/MyFitMain.js
@@ -4,14 +4,75 @@ import MyFit from "./MyFit";
 import ExerciseLog from "./ExerciseLog";
 import RoutineSetting from "./RoutineSetting";
 import ProfileCard from "./ProfileCard ";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { selectClasses } from "@mui/material";
 import dayjs from "dayjs";
 import "dayjs/locale/ko"; // 한글 locale 추가
+import axios from "axios";
 
 const MyFitMain = () => {
+  const [memberNo, setMemberNo] = useState(1);
+  const [record, setRecord] = useState(null);
+  const [routine, setRoutine] = useState(null);
+  const today = dayjs();
+
   const [date, setDate] = useState(dayjs());
   const [pageTitle, setPageTitle] = useState("My Fit");
+
+  const zeroDate =
+    String(date.$M + 1).length === 1 ? "0" + (date.$M + 1) : date.$M + 1;
+
+  const inputDate = dayjs(date.$y + "-" + (date.$M + 1) + "-" + date.$D);
+  const weekday = date.format("dddd").charAt(0);
+  const [title, setTitle] = useState(weekday + "요일 루틴");
+
+  const dateFormat =
+    date.$y + "-" + (date.$M + 1) + "-" + date.$D + "-" + weekday;
+
+  useEffect(() => {
+    if (inputDate.isBefore(today, "day")) {
+      setTitle("운동기록");
+      //과거이면 기록 조회
+      axios
+        .get(
+          `${
+            process.env.REACT_APP_BACK_SERVER
+          }/myfit/record/${memberNo}?recordDate=${
+            date.$y + "-" + zeroDate + "-" + date.$D
+          }`
+        )
+        .then((res) => {
+          setRecord(res.data);
+        })
+        .catch((err) => {});
+    } else {
+      if (inputDate.isSame(today, "day")) {
+        axios
+          .get(
+            `${
+              process.env.REACT_APP_BACK_SERVER
+            }/myfit/record/${memberNo}?recordDate=${
+              date.$y + "-" + zeroDate + "-" + date.$D
+            }`
+          )
+          .then((res) => {
+            setRecord(res.data);
+          })
+          .catch((err) => {});
+      }
+      setTitle(weekday + "요일 루틴");
+      //오늘이나 미래이면 루틴 조회
+      axios
+        .get(
+          `${process.env.REACT_APP_BACK_SERVER}/myfit/routine/${memberNo}?routineDay=${weekday}`
+        )
+        .then((res) => {
+          setRoutine(res.data);
+        })
+        .catch((err) => {});
+    }
+  }, [date]);
+
   return (
     <div className="myfit-wrap">
       <h1>{pageTitle}</h1>
@@ -19,9 +80,40 @@ const MyFitMain = () => {
         <Sidebar setPageTitle={setPageTitle} />
         <div className="myfit-content-box">
           <Routes>
-            <Route path="fit" element={<MyFit date={date} setDate={setDate} />} />
-            <Route path="record" element={<ExerciseLog />} />
-            <Route path="routine" element={<RoutineSetting date={date} />} />
+            <Route
+              path="fit"
+              element={
+                <MyFit
+                  date={date}
+                  setDate={setDate}
+                  setPageTitle={setPageTitle}
+                  memberNo={memberNo}
+                  setMemberNo={setMemberNo}
+                  record={record}
+                  setRecord={setRecord}
+                  routine={routine}
+                  setRoutine={setRoutine}
+                  today={today}
+                  title={title}
+                  inputDate={inputDate}
+                />
+              }
+            />
+            <Route
+              path="record"
+              element={<ExerciseLog dateFormat={dateFormat} />}
+            />
+            <Route
+              path="routine"
+              element={
+                <RoutineSetting
+                  memberNo={memberNo}
+                  date={date}
+                  routine={routine}
+                  setRoutine={setRoutine}
+                />
+              }
+            />
             <Route path="activity/*" element={<ProfileCard />} />
           </Routes>
         </div>
