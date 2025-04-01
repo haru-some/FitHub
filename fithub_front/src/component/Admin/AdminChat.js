a83d5de69caa1f207ee2439a9698359089fde7e2
codegangpay <allhoodgang@gmail.com>
2025-04-01 17:07:20 +0900

    feat, fix : AdminChat 추가 및 수정
    채팅 css 전반적인 수정 및 추가

--- fithub_front/src/component/Admin/AdminChat.js
+++ fithub_front/src/component/Admin/AdminChat.js
@@ -1,13 +1,17 @@
 import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import CircleIcon from "@mui/icons-material/Circle";
 import { useNavigate } from "react-router-dom";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { useState } from "react";
 
 const AdminChat = () => {
   const navigate = useNavigate();
   return (
-    <section className="chat-section">
+    <section className="admin-chat-section">
       <div className="page-title">문의 관리</div>
       <div className="admin-chat-box">
-        <div className="chat-member-list">
+        <div className="chat-list-box">
           <AdminChatList />
         </div>
         <div className="chat-box-view">
@@ -20,133 +24,25 @@ const AdminChat = () => {
 
 const AdminChatList = () => {
   return (
-    <div className="admin-chat-list">
-      <div className="admin-chat-room">
-        <div className="chat-member-profile">
+    <div className="chat-list">
+      <div className="chat-room">
+        <div className="member-chat-profile">
           <img src="/image/default_img.png" />
         </div>
-        <div className="chat-member-main">
-          <div className="chat-member-type">
+        <div className="chat-room-main">
+          <div className="member-chat-id">
             <div>회원 아이디</div>
           </div>
-          <div className="chat-member-content">
-            <div>마지막 문의 내용</div>
+          <div className="member-chat-content">
+            <div>
+              마지막 문의 내용 대충 여기가 마지막이라는 엄청 대단하진 않지만
+              그냥 멋진 말
+            </div>
           </div>
         </div>
-        <div className="chat-member-alarm">
-          <NotificationsActiveIcon />
-        </div>
-      </div>
-      <div className="admin-chat-room">
-        <div className="chat-member-profile">
-          <img src="/image/default_img.png" />
-        </div>
-        <div className="chat-member-main">
-          <div className="chat-member-type">
-            <div>회원 아이디</div>
-          </div>
-          <div className="chat-member-content">
-            <div>마지막 문의 내용</div>
-          </div>
-        </div>
-        <div className="chat-member-alarm">
-          <NotificationsActiveIcon />
-        </div>
-      </div>
-      <div className="admin-chat-room">
-        <div className="chat-member-profile">
-          <img src="/image/default_img.png" />
-        </div>
-        <div className="chat-member-main">
-          <div className="chat-member-type">
-            <div>회원 아이디</div>
-          </div>
-          <div className="chat-member-content">
-            <div>마지막 문의 내용</div>
-          </div>
-        </div>
-        <div className="chat-member-alarm">
-          <NotificationsActiveIcon />
-        </div>
-      </div>
-      <div className="admin-chat-room">
-        <div className="chat-member-profile">
-          <img src="/image/default_img.png" />
-        </div>
-        <div className="chat-member-main">
-          <div className="chat-member-type">
-            <div>회원 아이디</div>
-          </div>
-          <div className="chat-member-content">
-            <div>마지막 문의 내용</div>
-          </div>
-        </div>
-        <div className="chat-member-alarm">
-          <NotificationsActiveIcon />
-        </div>
-      </div>
-      <div className="admin-chat-room">
-        <div className="chat-member-profile">
-          <img src="/image/default_img.png" />
-        </div>
-        <div className="chat-member-main">
-          <div className="chat-member-type">
-            <div>회원 아이디</div>
-          </div>
-          <div className="chat-member-content">
-            <div>마지막 문의 내용</div>
-          </div>
-        </div>
-        <div className="chat-member-alarm">
-          <NotificationsActiveIcon />
-        </div>
-      </div>
-      <div className="admin-chat-room">
-        <div className="chat-member-profile">
-          <img src="/image/default_img.png" />
-        </div>
-        <div className="chat-member-main">
-          <div className="chat-member-type">
-            <div>회원 아이디</div>
-          </div>
-          <div className="chat-member-content">
-            <div>마지막 문의 내용</div>
-          </div>
-        </div>
-        <div className="chat-member-alarm">
-          <NotificationsActiveIcon />
-        </div>
-      </div>
-      <div className="admin-chat-room">
-        <div className="chat-member-profile">
-          <img src="/image/default_img.png" />
-        </div>
-        <div className="chat-member-main">
-          <div className="chat-member-type">
-            <div>회원 아이디</div>
-          </div>
-          <div className="chat-member-content">
-            <div>마지막 문의 내용</div>
-          </div>
-        </div>
-        <div className="chat-member-alarm">
-          <NotificationsActiveIcon />
-        </div>
-      </div>
-      <div className="admin-chat-room">
-        <div className="chat-member-profile">
-          <img src="/image/default_img.png" />
-        </div>
-        <div className="chat-member-main">
-          <div className="chat-member-type">
-            <div>회원 아이디</div>
-          </div>
-          <div className="chat-member-content">
-            <div>마지막 문의 내용</div>
-          </div>
-        </div>
-        <div className="chat-member-alarm">
-          <NotificationsActiveIcon />
+        <div className="member-chat-alarm">
+          <CircleIcon />
+          <span className="alarm-count">5</span>
         </div>
       </div>
     </div>
@@ -154,29 +50,66 @@ const AdminChatList = () => {
 };
 
 const AdminChatView = () => {
+  const [chatView, setChatView] = useState(false);
+  const [chatInput, setChatInput] = useState();
+  const messageSend = () => {
+    if (chatInput.trim() !== "") {
+      // onSendMessage(chatInput);
+
+      setChatInput("");
+    }
+  };
   return (
     <>
-      <div className="member-chat-list">
-        <div className="member-chat-room">
-          <div className="chat-member-profile">
+      <div className="chat-box">
+        <div className="member-chat-line">
+          <div className="member-chat-profile">
             <img src="/image/default_img.png" />
           </div>
-          <div className="chat-member-main">
-            <div className="chat-member-id">
-              <div>대충 킹조지</div>
+          <div className="member-chat-main">
+            <div className="member-chat-id">
+              <div>킹조지</div>
+              <div>2025-04-01</div>
+              {chatView ? <VisibilityIcon /> : <VisibilityOffIcon />}
             </div>
-            <div className="chat-member-content">
-              <div>
-                킹조지가 열변을 토하는 중
-                <span className="chat-alarm">(봤는지 안봤는지 숫자)</span>
+            <div className="member-chat-content">
+              <div className="member-chat-text">
+                킹조지가 열변을 토하는 중 킹조지가 열변을 토하는 중 킹조지가
+                열변을 토하는 중 킹조지가 열변을 토하는 중 킹조지가 열변을
+                토하는 중 킹조지가 열변을 토하는 중 킹조지가 열변을 토하는 중
+                킹조지가 열변을 토하는 중 킹조지가 열변을 토하는 중 킹조지가
+                열변을 토하는 중 킹조지가 열변을 토하는 중
               </div>
             </div>
           </div>
         </div>
+        <div className="admin-chat-line">
+          <div className="admin-chat-main">
+            <div className="admin-chat-id">
+              {chatView ? <VisibilityIcon /> : <VisibilityOffIcon />}
+              <div>2025-04-01</div>
+              <div>관리자</div>
+            </div>
+            <div className="admin-chat-content">
+              <div className="admin-chat-text">그냥 죽어</div>
+            </div>
+          </div>
+          <div className="admin-chat-profile">
+            <img src="/image/default_img.png" />
+          </div>
+        </div>
       </div>
-      <div className="member-input">
-        <input type="text" className="chat-input" />
-        <button type="button">전송</button>
+      <div className="chat-input-box">
+        <input
+          type="text"
+          className="chat-input"
+          value={chatInput}
+          onChange={(e) => setChatInput(e.target.value)}
+          placeholder="메시지를 입력해주세요."
+        />
+        <button type="button" onClick={messageSend}>
+          전송
+        </button>
       </div>
     </>
   );
