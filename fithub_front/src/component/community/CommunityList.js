3e77b034a5abfe487976c5e316773b4ccb7d782d
Nahanbin <nh5001@naver.com>
2025-04-01 10:23:22 +0900

    refactor : 하위 컴포넌트인 CommunityItem을 컴포넌트로 새로 만듬

--- fithub_front/src/component/community/CommunityList.js
+++ fithub_front/src/component/community/CommunityList.js
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from "react";
 import "./community.css"; // CSS 파일 불러오기
 
 import { Link, useNavigate } from "react-router-dom";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import ChatIcon from "@mui/icons-material/Chat";
+
 import PersonIcon from "@mui/icons-material/Person";
 import SearchIcon from "@mui/icons-material/Search";
 import CreateIcon from "@mui/icons-material/Create";
-import FavoriteIcon from "@mui/icons-material/Favorite";
+
 import axios from "axios";
 import { useRecoilState } from "recoil";
 import { loginIdState, memberState } from "../utils/RecoilData";
+import CommunityItem from "./CommunityItem";
 
 const CommunityList = () => {
   const [memberId, setMemberId] = useRecoilState(loginIdState);
@@ -44,12 +44,20 @@ const CommunityList = () => {
                   setShowInput(!showInput);
                 }}
               />
-              <CreateIcon
+              {member && (
+                <CreateIcon
+                  onClick={() => {
+                    navigate("/community/write");
+                  }}
+                />
+              )}
+
+              <PersonIcon
                 onClick={() => {
-                  navigate("/community/write");
+                  //1.내 memberNo 를 axios로 보내서 해당되는 리스트 불러옴
+                  //2. 커뮤니티리스트 스테이트에 셋
                 }}
               />
-              <PersonIcon />
             </div>
           </div>
           {showInput && (
@@ -84,104 +92,4 @@ const CommunityList = () => {
   );
 };
 
-const CommunityItem = (props) => {
-  const communityList = props.communityList;
-  const setCommunityList = props.setCommunityList;
-  const member = props.member;
-  const community = props.community;
-  const [isLike, setIsLike] = useState(community.isLike === 1);
-  const navigate = useNavigate();
-  console.log(community.isLike);
-
-  const changeLike = (e) => {
-    if (member) {
-      if (isLike) {
-        axios
-          .delete(
-            `${process.env.REACT_APP_BACK_SERVER}/community/${member.memberNo}?communityNo=${community.communityNo}`
-          )
-          .then((res) => {
-            const obj = communityList.filter(
-              (item, i) => item.communityNo === community.communityNo
-            )[0];
-            const idx = communityList.indexOf(
-              communityList.filter(
-                (item, i) => item.communityNo === community.communityNo
-              )[0]
-            );
-            obj["likeCount"] = res.data;
-            communityList[idx] = obj;
-
-            setCommunityList([...communityList]);
-            setIsLike(false);
-          });
-      } else {
-        axios
-          .post(
-            `${process.env.REACT_APP_BACK_SERVER}/community/${member.memberNo}?communityNo=${community.communityNo}`
-          )
-          .then((res) => {
-            const obj = communityList.filter(
-              (item, i) => item.communityNo === community.communityNo
-            )[0];
-            const idx = communityList.indexOf(
-              communityList.filter(
-                (item, i) => item.communityNo === community.communityNo
-              )[0]
-            );
-            obj["likeCount"] = res.data;
-            communityList[idx] = obj;
-
-            setCommunityList([...communityList]);
-            setIsLike(true);
-          });
-      }
-    }
-
-    e.stopPropagation();
-  };
-  return (
-    <li
-      className="community-post-item"
-      onClick={() => {
-        navigate(`/community/view/${community.communityNo}`);
-      }}
-    >
-      <div
-        className="user-info"
-        onClick={(e) => {
-          navigate("/myfit/activity");
-          e.stopPropagation();
-        }}
-      >
-        <div className="member-img">
-          <img src="/image/default_img.png"></img>
-        </div>
-        <div className="community-member">
-          <p>{community.memberId}</p>
-          {member && (
-            <button type="button" className="follow-btn">
-              팔로우
-            </button>
-          )}
-        </div>
-      </div>
-      <div
-        className="community-content-texteditor"
-        dangerouslySetInnerHTML={{ __html: community.communityContent }}
-      ></div>
-
-      <div className="community-sub-zone">
-        <div className="community-likes" onClick={changeLike}>
-          {isLike ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-          {community.likeCount}
-        </div>
-        <div className="community-comments">
-          <ChatIcon />
-          {community.commentCount}
-        </div>
-      </div>
-    </li>
-  );
-};
 export default CommunityList;
