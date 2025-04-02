092b922a761cc3de25f859b8c43f53b0552883ee
haru-some <777tmdgus@naver.com>
2025-04-02 10:06:04 +0900

    fix : db오류 수정
    - db오류 수정 (dependency 제거)
    - ChatController, ChatService CorsConfig 등 제거

--- fithub_back/src/main/java/kr/co/fithub/chat/model/service/ChatService.java
+++ fithub_back/src/main/java/kr/co/fithub/chat/model/service/ChatService.java
@@ -1,33 +1,8 @@
 package kr.co.fithub.chat.model.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import kr.co.fithub.chat.model.dto.ChatDTO;
-import kr.co.fithub.util.ChatMessageEntity;
-import kr.co.fithub.util.ChatMessageRepository;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 @Service
 public class ChatService {
 
-    @Autowired
-    private ChatMessageRepository chatMessageRepository;
-
-    public void saveMessage(ChatDTO message) {
-        ChatMessageEntity entity = new ChatMessageEntity();
-        entity.setChatRoomNo(message.getChatRoomNo());
-        entity.setUserId(message.getMemberId());
-        entity.setContent(message.getMessageContent());
-        entity.setMessageType(message.getMessageType());
-        entity.setSentAt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-        chatMessageRepository.save(entity);
-    }
-
-    public List<ChatMessageEntity> getMessages(Long chatRoomNo) {
-        return chatMessageRepository.findByChatRoomNoOrderBySentAt(chatRoomNo);
-    }
 }
