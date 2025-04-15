4f04dfe1bf74457c5250c52f3c114626fff7e329
Nahanbin <nh5001@naver.com>
2025-04-15 15:38:59 +0900

    fix : 로그아웃 시 mainpage로 이동, 로그인 없이 주소로 페이지 이동 시 로그인 필요한 페이지 로그인 페이지로 이동

--- fithub_front/src/component/community/CommunityMain.js
+++ fithub_front/src/component/community/CommunityMain.js
@@ -1,4 +1,4 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate, useParams } from "react-router-dom";
 import CommunityList from "./CommunityList";
 import CommunityView from "./CommunityView";
 import CommunityWrite from "./CommunityWrite";
@@ -9,27 +9,32 @@ import Swal from "sweetalert2";
 import AdBanners from "../utils/AdBanners";
 
 const CommunityMain = () => {
-  // const [logoutST, setLogoutST] = useRecoilState(logoutState);
-  // const [member, setMember] = useRecoilState(memberState);
-  // const navigate = useNavigate();
-  // if (logoutST) {
-  //   navigate("/");
-  //   setLogoutST(false);
-  // } else {
-  //   if (!member) {
-  //     Swal.fire({
-  //       title: "로그인 필요",
-  //       text: "로그인이 필요한 서비스입니다.",
-  //       icon: "warning",
-  //       confirmButtonColor: "#589c5f",
-  //       confirmButtonText: "확인",
-  //     }).then((result) => {
-  //       if (result.isConfirmed) {
-  //         navigate("/member/login");
-  //       }
-  //     });
-  //   }
-  // }
+  const params = useParams();
+  const [logoutST, setLogoutST] = useRecoilState(logoutState);
+  const [member, setMember] = useRecoilState(memberState);
+  const navigate = useNavigate();
+  console.log(params["*"]);
+  if (logoutST) {
+    navigate("/");
+    setLogoutST(false);
+  } else {
+    if (
+      !member &&
+      (params["*"].startsWith("write") || params["*"].startsWith("update"))
+    ) {
+      Swal.fire({
+        title: "로그인 필요",
+        text: "로그인이 필요한 서비스입니다.",
+        icon: "warning",
+        confirmButtonColor: "#589c5f",
+        confirmButtonText: "확인",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login");
+        }
+      });
+    }
+  }
   return (
     <div className="community-main-wrap">
       <div className="ads ads-set">
